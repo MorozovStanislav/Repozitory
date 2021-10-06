da8bc6b117ea27ac30d94ff7bed54acfb99ab00d
MorozovStanislav <91194323+MorozovStanislav@users.noreply.github.com>
2021-10-07 00:35:24 +0300

    Add files via upload

---
+++ Controllers/Provider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Shoe_store.Controllers
+{
+    [ApiController]
+    [Route("/Provider")]
+    public class ProviderController : ControllerBase
+    {
+
+        [HttpPost("Suppliesmaterials")]
+        public string Suppliesmaterials(string str)
+        {
+            return str; // Поставка материалов
+        }
+        [HttpPost("Suppliesshoes")]
+        public string Suppliesshoes(string str)
+        {
+            return str;// Поставка обуви
+        }
+    }
+}

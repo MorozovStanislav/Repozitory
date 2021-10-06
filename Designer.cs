d17a08c966dba9687a12030b83fb5978cfa13d1a
MorozovStanislav <91194323+MorozovStanislav@users.noreply.github.com>
2021-10-07 00:35:33 +0300

    Add files via upload

---
+++ Designer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Shoe_store.Controllers
+{
+    [ApiController]
+    [Route("/Designer")]
+    public class DesignerController : ControllerBase
+    {
+
+        [HttpPost("Createsdesignproject")]
+        public string Createsdesignproject(string str)
+        {
+            return str; // Создаёт дизайн-проект
+        }
+        [HttpPost("Definesmaterial")]
+        public string Definesmaterial(string str)
+        {
+            return str;// Определяет материал
+        }
+    }
+}
